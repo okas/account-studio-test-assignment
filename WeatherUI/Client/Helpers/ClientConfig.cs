8e24de9dcaa2bf9741f11f2ba3d34fe3a4a9e3bf
Lauri Saar <lauri.saar@outlook.com>
2022-09-01 17:22:23 +0100

    ADD(l10n): added settings-backed language selector feature

---
+++ WeatherUI/Client/Helpers/ClientConfig.cs
@@ -0,0 +1,8 @@
+﻿using System.Globalization;
+
+namespace WeatherUI.Client.Helpers;
+
+public class ClientConfig
+{
+    public IList<CultureInfo> SupportedCultures { get; set; } = Array.Empty<CultureInfo>();
+}
--- WeatherUI/Client/Program.cs
+++ WeatherUI/Client/Program.cs
@@ -1,11 +1,38 @@
+using System.Globalization;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.JSInterop;
 using WeatherUI.Client;
+using WeatherUI.Client.Helpers;
 
-var builder = WebAssemblyHostBuilder.CreateDefault(args);
+WebAssemblyHostBuilder builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services
+    .AddScoped(sp => new HttpClient { BaseAddress = new(builder.HostEnvironment.BaseAddress) })
+    .AddLocalization();
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.PostConfigureAll<ClientConfig>(myOptions =>
+{
+    string path = $"{nameof(ClientConfig)}:{nameof(ClientConfig.SupportedCultures)}";
+
+    myOptions.SupportedCultures = builder.Configuration[path]?
+        .Split(",")
+        .Select(c => new CultureInfo(c)).ToList()
+        ?? new List<CultureInfo>();
+    ;
+});
+
+IJSRuntime jsInterop = builder.Build().Services.GetRequiredService<IJSRuntime>();
+string appLanguage = await jsInterop.InvokeAsync<string>("appCulture.get");
+
+if (appLanguage != null)
+{
+    CultureInfo cultureInfo = new(appLanguage);
+    CultureInfo.DefaultThreadCurrentCulture = cultureInfo;
+    CultureInfo.DefaultThreadCurrentUICulture = cultureInfo;
+}
 
 await builder.Build().RunAsync();
