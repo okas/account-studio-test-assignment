c66107ded51dc40116fd6b1b81f38b19e5e0aecb
Lauri Saar <lauri.saar@outlook.com>
2022-09-01 17:56:29 +0100

    REFACTOR(forecast page): getting rid of dynamic code and Filed record:
    - Implementing proper L10N in .NET Core  way.

--- WeatherUI/Shared/PageViewItem.cs
+++ WeatherUI/Shared/PageViewItem.cs
@@ -3,14 +3,24 @@
 public class PageViewItem
 {
     public Guid Id { get; set; }
-
+    /// <summary>
+    /// Index for filed ordering.
+    /// </summary>
     public int Index { get; set; }
-
+    /// <summary>
+    /// Name of the objects's property of viewmodel's type.
+    /// </summary>
     public string Name { get; set; } = default!;
-
+    /// <summary>
+    /// Visibility control.
+    /// </summary>
     public bool Selected { get; set; }
-
+    /// <summary>
+    /// Page, where instance belongs.
+    /// </summary>
     public string PageId { get; set; } = default!;
-
+    /// <summary>
+    /// User to who instance belongs.
+    /// </summary>
     public string UserId { get; set; } = default!;
 }
