e0292b27a98259bfb7ff7c980ab9960d4a71d650
Lauri Saar <lauri.saar@outlook.com>
2022-09-01 01:35:24 +0100

    CHANGE(weather forecast controller): added support for query by id

--- WeatherUI/Server/Controllers/WeatherForecastController.cs
+++ WeatherUI/Server/Controllers/WeatherForecastController.cs
@@ -14,36 +14,20 @@ public class WeatherForecastsController : ControllerBase
     public WeatherForecastsController(ApiDbContext dbContext) => _dbContext = dbContext;
 
 
-    //private static readonly string[] Summaries = new[]
-    //{
-    //    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    //};
-
     [HttpGet]
     public async Task<IEnumerable<WeatherForecast>> GetAll()
     {
         return await _dbContext.WeatherForecasts.ToListAsync();
     }
 
-    //[HttpGet("{date}")]
-    //public WeatherForecast GetByDate(DateTime date)
-    //{
-    //    // TODO: temporal solution for reusability only.
-    //    TimeSpan diff = date - DateTime.Today;
 
-    //    return CreateWeatherForecast(diff.Days);
-    //}
-
-    //private static WeatherForecast CreateWeatherForecast(int index)
-    //{
-    //    return new WeatherForecast
-
-    //    {
-    //        Id = Guid.NewGuid(),
-    //        Date = DateTime.Now.AddDays(index),
-    //        TemperatureC = Random.Shared.Next(-20, 55),
-    //        Summary = Summaries[Random.Shared.Next(Summaries.Length)]
-    //    };
-    //}
-
+    [HttpGet("{id}")]
+    public async Task<ActionResult<WeatherForecast>> GetById(Guid id, CancellationToken ct)
+    {
+        return await _dbContext.WeatherForecasts
+            .AsNoTracking()
+            .SingleOrDefaultAsync(m => m.Id == id, ct).ConfigureAwait(false) is WeatherForecast model
+            ? Ok(model)
+            : NotFound();
+    }
 }
